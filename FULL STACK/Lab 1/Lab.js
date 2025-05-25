9069d44b03f2645d788241dfb1d091af8b3b94b0
Ishan <ishanahmed177@gmail.com>
2025-05-25 18:41:22 +0530

    deleted unneecessery files

--- FULL STACK/Lab 1/Lab.js
+++
@@ -1,8 +0,0 @@
-alert("hello");
-const num1 = parseInt(prompt("Enter the first number"));
-const num2 = parseInt(prompt("Enter the Second number"));
-const sum = num1 + num2;
-alert(sum);
-
-document.getElementById("demo").innerHTML =
-  "<h1> let us learn javascript </h1>";
